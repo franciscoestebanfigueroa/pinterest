4d67e30ec0f09d8d24514e78e4f9bf39776a1734
pancho <pancho@pancho.com>
2024-02-17 00:35:20 -0300

    |NO SE POR QUE SE REDIBUJAN LOS COLORES

--- lib/main.dart
+++ lib/main.dart
@@ -3,34 +3,34 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-  List<Item> get listadoDeMenu {
-    return [
-              Item(
-                icono: Icons.search,
-                function: (){print("hola 0");},
-                color: _Colores()
-               ),
-               Item(
-                icono: Icons.search,
-                function: (){print("hola 1");},
-                color: _Colores()
-               ),
-               Item(
-                icono: Icons.search,
-                function: (){print("hola 2");},
-                color: _Colores()
-               ),
-               Item(
-                icono: Icons.search,
-                function: (){print("hola 3");},
-                color: _Colores()
-               ),
-
-
-            ];
-  }
-
-
+List<Item> get listadoDeMenu {
+  return [
+    Item(
+        icono: Icons.search,
+        function: () {
+          print("hola 0");
+        },
+        color: _Colores()),
+    Item(
+        icono: Icons.search,
+        function: () {
+          print("hola 1");
+        },
+        color: _Colores()),
+    Item(
+        icono: Icons.search,
+        function: () {
+          print("hola 2");
+        },
+        color: _Colores()),
+    Item(
+        icono: Icons.search,
+        function: () {
+          print("hola 3");
+        },
+        color: _Colores()),
+  ];
+}
 
 void main() {
   runApp(const MyApp());
@@ -48,37 +48,32 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  Scaffold(
-        body: MyHomePageMenu()),
+      home: Scaffold(body: MyHomePageMenu()),
     );
   }
 }
 
-
 class MyHomePageMenu extends StatelessWidget {
-   MyHomePageMenu({super.key});
+  MyHomePageMenu({super.key});
   @override
   Widget build(BuildContext context) {
-
     return ChangeNotifierProvider(
-      create: (contex){return ProviderMenuFlotante();},
-      child: Builder(
-        builder: (context) {
-      bool estado=(Provider.of<ProviderMenuFlotante>(context).estadoMenu);
+      create: (contex) {
+        return ProviderMenuFlotante();
+      },
+      child: Builder(builder: (context) {
+        bool estado = (Provider.of<ProviderMenuFlotante>(context).estadoMenu);
 
-          return Stack(
-            children: [
-               FondoListado(),
-              estado? _MenuFlotante(Items: listadoDeMenu):SizedBox()
-            ],
-            );
-        }
-      ),
-      );
+        return Stack(
+          children: [
+            FondoListado(),
+            estado ? _MenuFlotante(Items: listadoDeMenu) : SizedBox()
+          ],
+        );
+      }),
+    );
   }
-
 }
-
 
 class FondoListado extends StatefulWidget {
   const FondoListado({
@@ -90,16 +85,17 @@ class FondoListado extends StatefulWidget {
 }
 
 class _FondoListadoState extends State<FondoListado> {
-
-final ScrollController controllerScroll =ScrollController();  
-@override
+  final ScrollController controllerScroll = ScrollController();
+  @override
   void initState() {
-
-    bool estadoM=Provider.of<ProviderMenuFlotante>(context,listen: false).estadoMenu;
-    controllerScroll.addListener(() { 
-      print(controllerScroll.offset);
-if(controllerScroll.offset<200.0){estadoM=false ;}else {estadoM=false;}
-
+    final estadoM = Provider.of<ProviderMenuFlotante>(context, listen: false);
+    controllerScroll.addListener(() {
+      //print(controllerScroll.offset);
+      if (controllerScroll.offset > 200.0) {
+        estadoM.estadoMenu = false;
+      } else {
+        estadoM.estadoMenu = true;
+      }
     });
     super.initState();
   }
@@ -111,121 +107,97 @@ if(controllerScroll.offset<200.0){estadoM=false ;}else {estadoM=false;}
     super.dispose();
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return     ListView.builder(
-      controller: controllerScroll,
-      itemBuilder: (context, index) => Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-        
-                      alignment: Alignment.bottomCenter,                  
-                      width: 150,
-                      height: 150,
-                      decoration: BoxDecoration(
-                      color: _Colores(),
-                      borderRadius: BorderRadius.circular(20)
-                    ),
-                      child: Text("$index"),),
-      ));
+    return ListView.builder(
+        controller: controllerScroll,
+        itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                    color: _Colores(), borderRadius: BorderRadius.circular(20)),
+                child: Text("$index"),
+              ),
+            ));
   }
 }
 
-class Item{
-   IconData icono;
-   VoidCallback function;
-   Color color;
-    
-    Item({
-      required this.icono,
-      required this.function,
-      this.color=Colors.blue
-      });
+class Item {
+  IconData icono;
+  VoidCallback function;
+  Color color;
 
-
-  
+  Item({required this.icono, required this.function, this.color = Colors.blue});
 }
 
 Color _Colores() {
-
   final Random random = Random();
 
-  return Color.fromRGBO(random.nextInt(256), random.nextInt(256), random.nextInt(256), 1);
-  
+  return Color.fromRGBO(
+      random.nextInt(256), random.nextInt(256), random.nextInt(256), 1);
 }
 
-
 class _MenuFlotante extends StatefulWidget {
-   List <Item>Items;
-   
-   _MenuFlotante({
-    required this.Items
-    });
+  List<Item> Items;
+
+  _MenuFlotante({required this.Items});
 
   @override
   State<_MenuFlotante> createState() => __MenuFlotanteState();
 }
 
-
 class __MenuFlotanteState extends State<_MenuFlotante> {
-  
-
-  
-  
   @override
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.bottomCenter,
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: 5.0),
-        margin:  const EdgeInsets.only(bottom: 40.0),
-        width: MediaQuery.of(context).size.width*.6,
+        margin: const EdgeInsets.only(bottom: 40.0),
+        width: MediaQuery.of(context).size.width * .6,
         decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-          boxShadow:  const <BoxShadow>[
-            BoxShadow(
-              color: Colors.black,
-              offset: Offset(0, 4),
-              blurStyle: BlurStyle.inner,
-              blurRadius: 80,
-              spreadRadius: 4,
-            ) 
-            ]
-        ),
-        
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: const <BoxShadow>[
+              BoxShadow(
+                color: Colors.black,
+                offset: Offset(0, 4),
+                blurStyle: BlurStyle.inner,
+                blurRadius: 80,
+                spreadRadius: 4,
+              )
+            ]),
         child: Row(
-
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children:List.generate(widget.Items.length, (index){
-          return  GestureDetector(
-            
-            onTap: widget.Items[index].function,
-            child: Icon(widget.Items[index].icono,color: widget.Items[index].color,));
-          }) ,
+          children: List.generate(widget.Items.length, (index) {
+            return GestureDetector(
+                onTap: widget.Items[index].function,
+                child: Icon(
+                  widget.Items[index].icono,
+                  color: widget.Items[index].color,
+                ));
+          }),
         ),
       ),
     );
-
   }
 }
 
+class ProviderMenuFlotante extends ChangeNotifier {
+  late bool _estadoMenu;
+  ProviderMenuFlotante() {
+    _estadoMenu = true;
+    print("hola");
+  }
 
-class ProviderMenuFlotante extends ChangeNotifier{
-  
-bool _estadoMenu=true;
+  bool get estadoMenu => _estadoMenu;
 
-bool get estadoMenu => _estadoMenu;
-
-set estadomenu (bool x){
-  _estadoMenu=x;
-  notifyListeners();
-  print(" estado del provider$_estadoMenu");
-}
-
-
-
-
-
+  set estadoMenu(bool x) {
+    _estadoMenu = x;
+    print(" estado del provider $_estadoMenu");
+    notifyListeners();
+  }
 }
